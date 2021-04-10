b0e92dd08186996500c85d304652159a19291d28
JF <jfan9988@gmail.com>
2021-04-10 13:20:13 +1000

    initial particle system

--- src/game.ts
+++ src/game.ts
@@ -4,82 +4,101 @@ const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // 
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
 const createScene = function () {
-  // This creates a basic Babylon Scene object (non-mesh)
   const scene = new BABYLON.Scene(engine);
-  scene.clearColor = BABYLON.Color4.FromColor3(BABYLON.Color3.Teal());
 
-  // This creates and positions a free camera (non-mesh)
-  const camera = new BABYLON.FreeCamera(
-    "camera1",
-    new BABYLON.Vector3(0, 5, -10),
+  // Setup environment
+  const camera = new BABYLON.ArcRotateCamera(
+    "ArcRotateCamera",
+    1,
+    0.8,
+    5,
+    new BABYLON.Vector3(0, 0, 0),
     scene
   );
-
-  // This targets the camera to scene origin
-  camera.setTarget(BABYLON.Vector3.Zero());
-
-  // This attaches the camera to the canvas
   camera.attachControl(canvas, true);
+  scene.clearColor = new BABYLON.Color4(0.0, 0.0, 0.0, 1);
 
-  // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  const light = new BABYLON.HemisphericLight(
-    "light1",
-    new BABYLON.Vector3(0, 1, 0),
+  // Create a particle system
+  const surfaceParticles = new BABYLON.ParticleSystem(
+    "surfaceParticles",
+    1600,
     scene
   );
 
-  // Default intensity is 1. Let's dim the light a small amount
-  light.intensity = 0.7;
-
-  // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-  const sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-  // Move the sphere upward 1/2 its height
-  sphere.position.y = 1;
-
-  // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-  const ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-
-  BABYLON.Effect.ShadersStore["customFragmentShader"] = `
-    #ifdef GL_ES
-        precision highp float;
-    #endif
-
-    // Samplers
-    varying vec2 vUV;
-    uniform sampler2D textureSampler;
-
-    // Parameters
-    uniform vec2 screenSize;
-    uniform float threshold;
-
-    void main(void)
-    {
-        vec2 texelSize = vec2(1.0 / screenSize.x, 1.0 / screenSize.y);
-        vec4 baseColor = texture2D(textureSampler, vUV);
-
-        if (baseColor.r < threshold) {
-            gl_FragColor = baseColor;
-        } else {
-            gl_FragColor = vec4(0);
-        }
-    }
-    `;
-
-  const postProcess = new BABYLON.PostProcess(
-    "My custom post process",
-    "custom",
-    ["screenSize", "threshold"],
-    null,
-    0.25,
-    camera
+  // Texture of each particle
+  surfaceParticles.particleTexture = new BABYLON.Texture(
+    "http://img1.wikia.nocookie.net/__cb20061003200043/uncyclopedia/images/4/44/White_square.png",
+    scene
   );
-  postProcess.onApply = function (effect) {
-    effect.setFloat2("screenSize", postProcess.width, postProcess.height);
-    effect.setFloat("threshold", 0.3);
-  };
 
-  console.log("hello");
+  // Create core sphere
+  const coreSphere = BABYLON.MeshBuilder.CreateSphere(
+    "coreSphere",
+    { diameter: 2.01, segments: 64 },
+    scene
+  );
+
+  // Create core material
+  const coreMat = new BABYLON.StandardMaterial("coreMat", scene);
+  coreMat.alpha = 0;
+
+  // Assign core material to sphere
+  coreSphere.material = coreMat;
+
+  // Pre-warm
+  surfaceParticles.preWarmStepOffset = 10;
+  surfaceParticles.preWarmCycles = 100;
+
+  // Initial rotation
+  surfaceParticles.minInitialRotation = -2 * Math.PI;
+  surfaceParticles.maxInitialRotation = 2 * Math.PI;
+
+  // Where the sun particles come from
+  const sunEmitter = new BABYLON.SphereParticleEmitter();
+  sunEmitter.radius = 1;
+  sunEmitter.radiusRange = -1; // emit only from shape surface
+
+  // Assign particles to emitters
+  surfaceParticles.emitter = coreSphere; // the starting object, the emitter
+  surfaceParticles.particleEmitterType = sunEmitter;
+
+  // Color gradient over time
+  surfaceParticles.color1 = new BABYLON.Color4(0.66, 0.66, 1, 1);
+  surfaceParticles.color2 = new BABYLON.Color4(0.37, 0.37, 1);
+
+  // Size of each particle
+  surfaceParticles.addSizeGradient(0, 0); //size at start of particle lifetime
+  surfaceParticles.addSizeGradient(0.25, 0.2);
+  surfaceParticles.addSizeGradient(0.75, 0.2);
+  surfaceParticles.addSizeGradient(1, 0); //size at end of particle lifetime
+
+  // Life time of each particle (random between...
+  surfaceParticles.minLifeTime = 1;
+  surfaceParticles.maxLifeTime = 4;
+
+  // Emission rate
+  surfaceParticles.emitRate = 200;
+
+  // Blend mode : BLENDMODE_ONEONE, BLENDMODE_STANDARD, or BLENDMODE_ADD
+  surfaceParticles.blendMode = BABYLON.ParticleSystem.BLENDMODE_ADD;
+
+  // Set the gravity of all particles
+  surfaceParticles.gravity = new BABYLON.Vector3(0, 0, 0);
+
+  // Angular speed, in radians
+  surfaceParticles.minAngularSpeed = -0.4;
+  surfaceParticles.maxAngularSpeed = 0.4;
+
+  // Speed
+  surfaceParticles.minEmitPower = -1;
+  surfaceParticles.maxEmitPower = 0;
+  surfaceParticles.updateSpeed = 0.005;
+
+  // No billboard
+  surfaceParticles.isBillboardBased = false;
+
+  // Start the particle system
+  surfaceParticles.start();
 
   return scene;
 };
